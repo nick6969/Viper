fe182f1b107159af93bb3178e77d8435e4f3cd19
nick <hawhaw.ya@gmail.com>
2020-05-06 09:27:20 +0800

    first commit

---
+++ VIPER-Talk/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  VIPER-Talk
+//
+//  Created by Nick on 5/6/20.
+//  Copyright © 2020 co.Kcin.Nil. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+}
---
+++ VIPER-TalkTests/VIPERTalkTests.swift
@@ -0,0 +1,14 @@
+//
+//  VIPER_TalkTests.swift
+//  VIPER-TalkTests
+//
+//  Created by Nick on 5/6/20.
+//  Copyright © 2020 co.Kcin.Nil. All rights reserved.
+//
+
+import XCTest
+@testable import VIPER_Talk
+
+class VIPERTalkTests: XCTestCase {
+
+}
